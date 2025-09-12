553f70875b8f443cea6df145533689b591b3d208
Reduzer <83287196+Reduzer@users.noreply.github.com>
2025-09-12 12:38:01 +0200

    Changes

---
+++ Terminkalender/Shared/WochenTermin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+	public class WochenTermin : ITermin
+	{
+		private List<Termin> m_voTermineDerWoche;
+
+		public WochenTermin()
+		{
+		
+		}
+
+		public void AddTermin(Termin oTermin)
+		{
+			m_voTermineDerWoche.Add(oTermin);
+		}
+
+		public IEnumerable<object> GetGuests()
+		{
+			throw new NotImplementedException();
+		}
+
+		public string GetName() 
+		{
+			
+		}
+	}
+}
