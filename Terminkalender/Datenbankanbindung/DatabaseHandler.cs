b96ae060af638d2ce02cb43e4e0b2977915fc642
Fyn <thereduzer897@gmail.com>
2025-09-19 12:14:10 +0200

    Changes
    Started to work on the Database integration
    (Only pushed as i want to switch devices for worken further)

---
+++ Terminkalender/Datenbankanbindung/DatabaseHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Datenbankanbindung
+{
+	public class DatabaseHandler
+	{
+		private static DatabaseHandler instance;
+
+		private DatabaseHandler()
+		{
+		
+		}
+
+		public static DatabaseHandler Instance
+		{
+			get{
+				if(instance == null){
+					instance = new DatabaseHandler();
+				}
+
+				return instance;
+			}
+		}
+	}
+}
