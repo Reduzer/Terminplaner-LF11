ed31b9146702d6febe568f2f0749ad56139189a8
BjarneObst <143389462+BjarneObst@users.noreply.github.com>
2025-09-22 09:41:36 +0200

    Neuen Termin anlegen page erstellt

--- Terminkalender/Terminkalender/StartPage.xaml.cs
+++ Terminkalender/Terminkalender/StartPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,20 +16,13 @@ using System.Windows.Shapes;
 namespace Terminkalender
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaktionslogik für StartPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class StartPage : Page
     {
-        public MainWindow()
+        public StartPage()
         {
             InitializeComponent();
-            var fp = new TerminAnlegen ();
-            NavFrame.Navigate(fp);
-        }
-
-        public void SetPage(Page nextPage)
-        {
-            NavFrame.Navigate(nextPage);
         }
     }
 }
