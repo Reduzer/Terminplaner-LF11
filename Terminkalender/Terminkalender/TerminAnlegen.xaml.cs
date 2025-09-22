35eafc3323fcc330252df592c5828ce881826d1d
Lysander <lysander2004.lh@gmail.com>
2025-09-22 19:54:30 +0200

    Mapper zu TerminAnlegen hinzugefügt

--- Terminkalender/Terminkalender/TerminAnlegen.xaml.cs
+++ Terminkalender/Terminkalender/TerminAnlegen.xaml.cs
@@ -35,22 +35,56 @@ namespace Terminkalender
 			}
 		}
 
+        private List<string> GetParticipantsFromBox()
+        {
+            string sBaseString = TeilnehmerBox.Text;
+
+            List<string> vsNames = new List<string>();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < sBaseString.Length; i++)
+            {
+                if (sBaseString[i] == ' ')
+                {
+                    continue;
+                }
+                else if (sBaseString[i] != ',')
+                {
+                    sb.Append(sBaseString[i]);
+                }
+                else
+                {
+                    vsNames.Add(sb.ToString());
+                    sb.Clear();
+                }
+            }
+
+            vsNames.Add(sb.ToString());
+            return vsNames;
+        }
+
         private void SavedClick(object sender, RoutedEventArgs e)
         {
             
             String title = TitleBox.Text;
-            DateTime? date = DateTime.Parse(DatePicker.Text);
-            //Was ist mit Von und Bis
-            //TeilnehmerListe
+            DateTime date = DateTime.Parse(DatePicker.Text);
+            DateTime startHour = DateTime.Parse(StartBox.Text);
+            DateTime endHour = DateTime.Parse(EndBox.Text);
+            List<Participant> participants = new List<Participant>();
+
+            foreach(String sTemp in GetParticipantsFromBox()) {
+                participants.Add(new Participant(sTemp));
+            }
+
             String discription = DescriptionBox.Text;
 
-            if (title != String.Empty && date != null) {
+            if (title == String.Empty && participants.Count == 0) {
                 MessageBox.Show("Titel, Wiederholung oder Datum sind null", "Info");
                 return;
             }
-            //Termin neuerTermin = new Termin(title, date, );
+            Termin neuerTermin = new Termin(title, startHour, endHour, participants);
 
-            
+
         }
     }
 }
